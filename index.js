a47118b5dd9db2bcf81bc6836fff1b2b83a8063c
kerperryjr <keriperryjr@gmail.com>
2023-04-09 20:13:25 -0500

    added more teacher starter code

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 
+let quizDiv = document.querySelector("quiz");
+let questionButton1 = document.querySelector("#answer1");
 
-
+//timer needs to be rendered to the page
 let timer = 90
 
 
@@ -8,25 +10,39 @@ let timerId = setInterval(function(){
 
     timer -= 1
     console.log(timer);
-    clearInterval(timerId)
+
 }, 1000)
 
 
 
-
+// insert correct questions render questions to the page
 let questions = [{question: "which pokemon does Ash get at the start of pokemon?", answers:["charmander", "susu", "juju", "pikachu"], correctAnswer: "pikachu" },
     {questions: "which pokemon deals ?", answers:["charmander", "jojo", "bobo", "pikachu"], correctAnswer: "pikachu"}
 ]
 
 let currentQuestion = 0
-
+renderQuestion()
 
 function renderQuestion(){
 
-    console.log(questions[0].questions)
-    console.log(questions[0].answers[0])
-    console.log(questions[0].answers[1])
-    console.log(questions[0].answers[2])
-    console.log(questions[0].answers[3])
+    console.log(questions[currentQuestion].questions)
+    questionButton1.textContent = questions[currentQuestion].answers[0];
+    console.log(questions[currentQuestion].answers[1])
+    console.log(questions[currentQuestion].answers[2])
+    console.log(questions[currentQuestion].answers[3])
     console.log("correct answer:" + questions[currentQuestion].correctAnswer)
 }
+
+//display my button id
+quizDiv.addEventListener("click", function(event){
+    if(event.target.matches("button")){
+        console.log("clicked!")
+        
+        renderQuestion();
+        currentQuestion++
+
+    }
+
+})
+
+
